784c886674c4d916435d1225e10600154a6047cd
Andrew McInally <realfunnyestate@yahoo.com>
2022-07-11 07:56:16 -0600

    refactored fetching users

--- src/context/github/GithubReducer.js
+++ src/context/github/GithubReducer.js
@@ -1,12 +1,17 @@
 const githubReducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'GET_USERS':
       return {
         ...state,
         users: action.payload,
         loading: false,
       }
-    default: 
+    case 'SET_LOADING':
+      return {
+        ...state,
+        loading: true,
+      }
+    default:
       return state
   }
 }
