f0cf5e8805fe9a9234719cc97293b3617bb9a2b0
Andrew McInally <realfunnyestate@yahoo.com>
2022-07-25 12:59:19 -0400

    added more icons

--- src/components/repos/RepoItem.jsx
+++ src/components/repos/RepoItem.jsx
@@ -25,6 +25,15 @@ function RepoItem({ repo }) {
           <div className="mr-2 badge badge-info badge-lg">
             <FaEye className="mr-2" /> {watchers_count}
           </div>
+          <div className="mr-2 badge badge-success badge-lg">
+            <FaStar className="mr-2" /> {stargazers_count}
+          </div>
+          <div className="mr-2 badge badge-error badge-lg">
+            <FaInfo className="mr-2" /> {open_issues}
+          </div>
+          <div className="mr-2 badge badge-info badge-lg">
+            <FaUtensils className="mr-2" /> {forks}
+          </div>
         </div>
       </div>
     </div>
