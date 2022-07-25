bb9c06f1fffdc45317a35b5fe4f23c10094d856d
Andrew McInally <realfunnyestate@yahoo.com>
2022-07-25 13:41:48 -0400

    refactored

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -17,25 +17,6 @@ export const GithubProvider = ({ children }) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState)
 
-  //get search results
-  const searchUsers = async (text) => {
-    setLoading()
-
-    const params = new URLSearchParams({ q: text })
-
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
-      headers: {
-        Authorization: `token ${GITHUB_TOKEN}`,
-      },
-    })
-
-    const { items } = await response.json()
-
-    dispatch({
-      type: 'GET_USERS',
-      payload: items,
-    })
-  }
   //get single user
   const getUser = async (login) => {
     setLoading()
@@ -91,11 +72,9 @@ export const GithubProvider = ({ children }) => {
   return (
     <GithubContext.Provider
       value={{
-        users: state.users,
-        user: state.user,
-        loading: state.loading,
-        repos: state.repos,
-        searchUsers,
+        ...state,
+        dispatch,
+
         clearUsers,
         getUserRepos,
         getUser,
