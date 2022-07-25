4a43490b6ba7982137bed48e6132ad2d90cbbc34
Andrew McInally <realfunnyestate@yahoo.com>
2022-07-25 14:57:17 -0400

    refactoring

--- src/pages/User.jsx
+++ src/pages/User.jsx
@@ -5,15 +5,23 @@ import RepoList from '../components/repos/RepoList'
 import { useEffect, useContext } from 'react'
 import { useParams } from 'react-router-dom'
 import GithubContext from '../context/github/GithubContext'
+import { getUser, getUserRepos } from '../context/github/GithubActions'
 
 function User() {
-  const { getUser, user, getUserRepos, loading, repos } = useContext(GithubContext)
+  const { user, loading, repos, dispatch } = useContext(GithubContext)
 
   const params = useParams()
 
   useEffect(() => {
-    getUser(params.login)
-    getUserRepos(params.login)
+    dispatch({ type: 'SET_LOADING' })
+    const getUserData = async () => {
+      const userData = await getUser(params.login)
+      dispatch({ type: 'GET_USER', payload: userData })
+
+      const userRepoData = await getUserRepos(params.login)
+      dispatch({ type: 'GET_REPOS', payload: userRepoData })
+    }
+    getUserData()
   }, [])
 
   const {
@@ -154,7 +162,7 @@ function User() {
             </div>
           </div>
         </div>
-        <RepoList repos={repos}  />
+        <RepoList repos={repos} />
       </div>
     </>
   )
